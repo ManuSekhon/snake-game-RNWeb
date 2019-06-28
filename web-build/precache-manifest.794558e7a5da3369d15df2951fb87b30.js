3468b516d74f133d6a4214c11250f97cfde29469
manuinder <manuinder@geekyants.com>
2019-06-28 19:48:50 +0530

    github pages

---
+++ web-build/precache-manifest.794558e7a5da3369d15df2951fb87b30.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "db5403893b21fd749b69",
+    "url": "/static/js/app.24d0e455.chunk.js"
+  },
+  {
+    "revision": "7e4467d9ebe2844f522c",
+    "url": "/static/js/runtime~app.2dbaae62.js"
+  },
+  {
+    "revision": "095c00c0daefd1e3e426e3692361fa9a",
+    "url": "/static/media/game-over-sound.095c00c0.mp3"
+  },
+  {
+    "revision": "af6955a3dc4b3e5f780bae4ff7d96a4d",
+    "url": "/static/media/eating-sound.af6955a3.mp3"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "6edeb1c26bf1032582ba8d91366435b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b3e13cc1fffce7a969aeb053d2468a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "28fccb76bdb96b188006",
+    "url": "/static/js/2.eadd8134.chunk.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  }
+];
